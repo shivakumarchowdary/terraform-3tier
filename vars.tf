c69ef84c4f6542cd0330ece065c6c9aae8e5a232
shivakumarchowdary <mandapalli43@gmail.com>
2024-12-26 10:14:56 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,27 @@
+variable "VPC_CIDR" {
+default ="10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+default ="10.0.1.0/24"
+}
+
+variable "subnet1_cidr" {
+default="10.0.2.0/24"
+}
+
+variable "subnet2_cidr" {
+default ="10.0.3.0/24"
+}
+
+variable "subnet3_cidr" {
+default="10.0.4.0/24"
+}
+
+variable "subnet4_cidr" {
+default="10.0.5.0/24"
+}
+
+variable "subnet5_cidr" {
+default="10.0.6.0/24"
+}
