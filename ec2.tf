afcb98fb4b895467cc27001df91c7c55581ed37e
shivakumarchowdary <mandapalli43@gmail.com>
2024-12-26 10:17:41 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "public_subnet-1" {
+ami="ami-012967cc5a8c9f891"
+instance_type="t2.micro"
+count=1
+key_name="pinku"
+vpc_security_group_ids=["${aws_security_group.demosg.id}"]
+subnet_id="${aws_subnet.public_subnet-1.id}"
+associate_public_ip_address=true
+user_data="${file("data.sh")}"
+tags={
+Name="My public Instance 1 "
+}
+}
+
+resource "aws_instance" "public_subnet-2" {
+ami="ami-012967cc5a8c9f891"
+instance_type="t2.micro"
+count=1
+key_name="pinku"
+vpc_security_group_ids=["${aws_security_group.demosg.id}"]
+subnet_id="${aws_subnet.public_subnet-2.id}"
+associate_public_ip_address=true
+user_data="${file("data.sh")}"
+tags={
+Name="My public Instance 2 "
+}
+}
